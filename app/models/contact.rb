93a2a6acdd993e839ff8897b10232fc07b339ca6
Antonio Samuels <design@antoniosamuels.com>
2018-08-26 23:02:14 -0400

    add file upload

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,7 @@ class Contact < MailForm::Base
     attribute :name, :validate => true
     attribute :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
     attribute :phone, :validate => true
+    attribute :file,      :attachment => true
     attribute :message, :validate => true
     attribute :nickname, :captcha => true
 
